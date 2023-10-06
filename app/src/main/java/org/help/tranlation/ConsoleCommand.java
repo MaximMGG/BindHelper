ddb69016161efe2730e55efaa51a5ecaf7aeb0ca
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-10-06 14:21:41 +0300

    init poject

---
+++ app/src/main/java/org/help/tranlation/ConsoleCommand.java
@@ -0,0 +1,28 @@
+package org.help.tranlation;
+
+import java.io.Console;
+
+public class ConsoleCommand {
+
+
+
+    public void run(String word) {
+        askGoogleTrans(word);
+
+    }
+
+    public void askGoogleTrans(String word) {
+        String[] command = new String[2];
+        command[0] = "chrome.exe";
+        String com2 = "https://translate.google.com/?sl=en&tl=ru&text=" + word + "%0A&op=translate";
+        command[1] = com2;
+
+        try {
+            Runtime.getRuntime().exec(command);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
